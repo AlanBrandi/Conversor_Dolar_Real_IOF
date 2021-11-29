05dfbc39c13978bcf949036236b55d47d710cd02
Alan Brandi <alan.brandi@hotmail.com>
2021-11-29 15:01:27 -0300

    Criação do programa.

---
+++ Conversor/Conversor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Conversor
+{
+    class Conversor
+    {
+        //Variáveis utilizadas dentro da classe conversor
+        private double TotalemReais;
+        private double TotalComTaxa;
+        private double ValorTaxa;
+        public double IOF = 0.0408;
+
+        //Encapsulamento Cotação e Quantidade de dólares.
+        public double CotacaoDolar { get; set; }
+        public double QuantidadeDolar { get; set; }
+
+        //Construtor personalizado.
+        public Conversor(double cotacaoDolar, double quantidaDolar)
+        {
+            CotacaoDolar = cotacaoDolar;
+            QuantidadeDolar = quantidaDolar;
+        }
+        //Método conversão sem taxa.
+        public void ConversaoSemTaxa()
+        {
+            TotalemReais = CotacaoDolar * QuantidadeDolar;
+        }
+        //Método conversão com taxa.
+        public void ConversaoComTaxa()
+        {
+            ValorTaxa = (TotalemReais * IOF);
+            TotalComTaxa = ValorTaxa + TotalemReais;
+        }
+        //Override ToString.
+        public override string ToString()
+        {
+            return "Valor a ser pago em reais = " + TotalemReais.ToString("C2") + "\nValor a ser pago de IOF = " + ValorTaxa.ToString("C2") + "(4,08%) " + "\nValor total da compra com IOF = " + TotalComTaxa.ToString("C2");
+        }
+    }
+}
---
+++ Conversor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Perguntando a cotação do dólar atual + salvando em "ValorDolar".
+            Console.Write("Qual a cotação do dólar atual? $");
+            double ValorDolar = double.Parse(Console.ReadLine());
+            //Perguntando a quantidade de dólares que o usuário irá comprar + salvando em "QuantidadeDolar".
+            Console.Write("\nQuantos dólares você irá comprar? $");
+            double QuantidadeDolar = double.Parse(Console.ReadLine());
+            //Instanciando um novo conversor, junto de suas variáveis necessárias.
+            Conversor convertor = new Conversor(ValorDolar, QuantidadeDolar);
+            //Executando método comversão sem taxa e conversão com taxa.
+            convertor.ConversaoSemTaxa();
+            convertor.ConversaoComTaxa();
+            //Mostrando os resultados na tela.
+            Console.WriteLine(convertor.ToString());
+        }
+    }
+}
